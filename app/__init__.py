76c10a3e2dd4e5a26d20ff95da30adb3822556b1
David Alexandre Hébert Aguilar <david.hebertaguilar@dawsoncollege.qc.ca>
2025-04-16 12:25:28 -0400

    update base.html and css. Set up home.html and it runs

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,10 @@
 from flask import Flask
-from config import Config
 from flask_login import LoginManager
-from models.database import db
 from .main.routes import main
 
 
 
 def create_app():
     app = Flask(__name__)
+    app.register_blueprint(main)
     return app
