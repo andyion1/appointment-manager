3e3d0ae85e01bd3d26e2b3f0204362d0a1e85ee8
Andy Ionita <andyionita06@gmail.com>
2025-04-23 20:16:15 -0400

    Make the necessary html block code, fix the bugs for html by adding a security key, and making the css for everything that is form-related

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_login import LoginManager
+from config import Config
 from .main.routes import main
 from .appointment.routes import appointment
 from .report.routes import report
@@ -8,9 +9,15 @@ from .student.routes import student
 
 def create_app():
     app = Flask(__name__)
+    
+    # Load configuration
+    app.config.from_object(Config)
+    
+    # Register blueprints
     app.register_blueprint(main)
     app.register_blueprint(appointment)
     app.register_blueprint(report)
     app.register_blueprint(user)
     app.register_blueprint(student)
+    
     return app
