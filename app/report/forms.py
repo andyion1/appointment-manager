f9a0417bc9a09b9c4336f82755fcdc3f132b41d2
unknown <joshuachaisson3@gmail.com>
2025-05-14 02:34:45 -0400

    Revert "Merge branch 'appointment' into 'main'"
    This reverts commit a1677303d3c1f0838af90a25996a4f9649af0cc0, reversing
    changes made to 45d5edd3ea5623272c710ff4408e03daef7538ef.

--- app/report/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import TextAreaField, SubmitField, HiddenField
-from wtforms.validators import DataRequired
-
-class ReportForm(FlaskForm):
-    appointment_id = HiddenField('Appointment ID')
-    feedback = TextAreaField('Student Feedback')
-    teacher_response = TextAreaField('Teacher Response')
-    submit = SubmitField('Submit Report')
